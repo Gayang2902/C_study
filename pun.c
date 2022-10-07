fa8952f4bee14fe33fb4d757644ac8f2a625ae38
K_DOUK03 <caley329@naver.com>
2022-10-07 21:17:31 +0900

    test

--- pun.c
+++ pun.c
@@ -2,7 +2,5 @@
 
 int main(void)
 {
-    printf("To C, or not to C: that is the question. \n");
-    printf("Brevity is the soul of wit.\n --Shakespeare\n");
-    return 0;
+    printf("Hello World!\n");
 }
