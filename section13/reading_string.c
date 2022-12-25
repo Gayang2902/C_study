ef90584e2e10cd4efdcc115fda5b74dbc07ff21c
K_DOUK03 <oneorz3601@gmail.com>
2022-12-26 01:59:32 +0900

    section13 except project

---
+++ section13/reading_string.c
@@ -0,0 +1,14 @@
+int read_line(char str[], int n)
+{
+    int ch;
+    int i = 0;
+
+    while ((ch = getchar()) != '\n') {
+        if (i < n) {
+            str[i++] = ch;
+        }
+    }
+
+    str[i] = '\0';
+    return i;
+}
